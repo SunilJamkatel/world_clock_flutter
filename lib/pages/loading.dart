c7d16618634489d110e4211fc3eebfa5c14864a3
Sunil Jamkatel <sunil.jamkatel@gmail.com>
2019-09-27 16:51:56 -0500

    init files

---
+++ lib/pages/loading.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:world_clock/services/world_time.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Loading screen"),
+    );
+  }
+}
